5cdb4f2a293c4e1f743feea10609e93e91eff9eb
Garrett Pace <elderpace6@gmail.com>
2023-04-13 13:01:41 -0600

    whew!

--- making-decisions-2.js
+++ making-decisions-2.js
@@ -2,6 +2,7 @@
 let backpack = [];
 let belt = [];
 let equippedItems = [];
+let saddleBag = []
 
 backpack.push("sword", "shield", "food");
 
@@ -14,4 +15,47 @@ backpack.unshift(furCoat);
 
 equippedItems.push(backpack.splice(0, 1)[0]);
 
-console.log(backpack, belt, equippedItems);
+backpack.push("flint", "blanket", "knife", "toothbrush");
+
+saddleBag.push(...backpack.splice(2, 3));
+
+let backpackCount = backpack.length;
+
+// console.log(backpack, backpackCount, belt, equippedItems, saddleBag);
+// console.log(saddleBag[1]);
+
+backpack.push("nintendo switch", "torch")
+
+for(let i=0; i<backpack.length; i++){
+    if(i >= 3){
+        let overflow = backpack.splice(3);
+    saddleBag.push(...overflow);
+    }
+}
+
+let firstThree = saddleBag.slice(0, 3)
+let lastThree = saddleBag.slice(saddleBag.length - 3)
+console.log(firstThree, lastThree)
+console.log(saddleBag);
+
+
+for(let i=0; i<saddleBag.length; i++){
+// console.log(saddleBag[i])
+}
+
+let guessMe = 54
+let count = 0
+
+while(guessMe < 12000){
+console.log('-------------');
+if(guessMe % 4 ===0 || guessMe % 5 ===0){
+    guessMe += 25
+}else if(guessMe %3 ===0){
+    guessMe -= 27
+}else{(guessMe)+=3
+}
+guessMe += 22
+count++;
+}
+
+console.log(`final number is ${guessMe}`, `the loop ran ${count} times`)
